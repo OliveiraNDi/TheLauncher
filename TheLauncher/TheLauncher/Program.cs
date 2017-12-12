b6d0d4a8e033523be9dde242d5629db6038af740
OliveiraNDi <diogoandre.oliveiranunes@rpn.ch>
2017-12-12 16:20:44 +0100

    Ajout de nouvelles commandes|Les fonctions ne sont pas encore créée

--- TheLauncher/TheLauncher/Program.cs
+++ TheLauncher/TheLauncher/Program.cs
@@ -11,14 +11,54 @@ namespace TheLauncher
     class Program
     {
 
-        const string pathFile = "..\\..\\mcFile.txt";
-        const string pathFolder = "..\\..\\mcFolder.txt";
-
+        const string pathApp = "..\\..\\McApp.txt";
+        const string pathFolder = "..\\..\\McFolder.txt";
+        const string Config = "..\\..\\Config.txt";
         static void Main(string[] args)
         {
-            File.AppendAllText(pathFile, "");
+            File.AppendAllText(pathApp, "");
             File.AppendAllText(pathFolder, "");
+            Console.WriteLine("---------------------------------------------------------");
+            Console.WriteLine("-\tPour voir toutes les commandes faites \"help\"\t-");
+            Console.WriteLine("---------------------------------------------------------");
+            string strSaisie = "";
+            do
+            {
+                Console.WriteLine("Run");
+                Console.Write("$");
+                strSaisie = (Console.ReadLine());
 
+                switch(strSaisie)
+                {
+                    case "help":
+                                                // Call HelpText()
+                        break;
+                    case "list":
+                                                // Call ReadFilesList()
+                        break;
+                    case "add":
+                        
+                        break;
+                    case "add -a":
+                    case "add -f":
+                        if (strSaisie == "add -a")
+                        {
+                            // Call WriteMcApp(add -a)
+                        }
+                        else
+                        {
+                            // Call WriteMcFile(add -f)
+                        }
+                        break;
+                    case "config":
+                        break;
+                    default:
+                            // Call ReadLines(strSaisie)
+                        break;
+
+                        
+                }
+            } while (strSaisie != "exit");
         }
 
     }
