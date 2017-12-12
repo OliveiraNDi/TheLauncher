2979eea66d33082fa6c53f4cb56e043f388d4d97
OliveiraNDi <diogoandre.oliveiranunes@rpn.ch>
2017-12-12 14:37:04 +0100

    Mis un code qui créer des fichiers de sauvegarde

---
+++ TheLauncher/TheLauncher/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheLauncher
+{
+    class Program
+    {
+
+        const string pathFile = "..\\..\\mcFile.txt";
+        const string pathFolder = "..\\..\\mcFolder.txt";
+
+        static void Main(string[] args)
+        {
+            File.AppendAllText(pathFile, "");
+            File.AppendAllText(pathFolder, "");
+
+        }
+
+    }
+}
